af0273a1deb086aad566186426c7d9ad59e11f1a
Leo Bruschi <bruschileopoldo@gmail.com>
2024-08-28 09:49:51 -0300

    Inicio

---
+++ ListaExercicios/Ex06b/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int tamanho = 100;
+        int[] vetor = new int[tamanho];
+
+        Random random = new Random();
+
+        // Preenchendo o vetor com valores aleatórios e exibindo
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = random.Next(1000);
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Ordenando o vetor usando Array.Sort em uma linha
+        Array.Sort(vetor); //Array.Sort(vetor, (a, b) => b.CompareTo(a)); - ordem decrescente 
+
+        // Exibindo o vetor ordenado
+        Console.WriteLine();
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ ListaExercicios/Exe06a/Program.cs
@@ -0,0 +1,59 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int tamanho = 100;
+        int[] vetor = new int[tamanho];
+
+        Random random = new Random();
+
+        // Preenchendo o vetor com valores aleatórios e exibindo
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = random.Next(1000);
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine();
+
+        // Ordenando o vetor usando Bubble Sort
+        for (int i = 0; i < vetor.Length - 1; i++)
+        {
+            for (int j = 0; j < vetor.Length - 1 - i; j++)
+            {
+                if (vetor[j] > vetor[j + 1])
+                {
+                    // Troca os elementos
+                    int aux = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = aux;
+                }
+            }
+        }
+    Console.Write("\n");
+        // Exibindo o vetor ordenado
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+//bool troca;
+//do{
+    //	troca = false;
+//for (int i = 0; i < vetor.Length - 1; i++)
+      //  {
+        //    for (int j = 0; j < vetor.Length - 1 - i; j++)
+        //    {
+          //      if (vetor[j] > vetor[j + 1])
+         //       {
+		//    troca = true;
+       //             int aux = vetor[j];
+       //             vetor[j] = vetor[j + 1];
+      //              vetor[j + 1] = aux;
+       //         }
+   //         }
+//} while(troca == true);
